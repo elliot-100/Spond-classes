2b5b2d0fb8ce440b2b200fdcaca8ade9f7ad3d09
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2023-02-21 20:29:53 +0000

    test: test complex attributes of child classes within `SpondGroup` tests.

--- tests/test_spondgroup.py
+++ tests/test_spondgroup.py
@@ -1,7 +1,5 @@
-""" Tests for SpondGroup class.
-
-Note: To generate a representative 32-character hex string ID:
-    secrets.token_hex(16).upper()
+"""
+Tests for SpondGroup class and child classes.
 """
 
 import pytest
@@ -33,8 +31,10 @@ def test_create():
 
 @pytest.fixture
 def simplest_group_dict():
-    """Partial fragment from the 'groups' (root) node.
-    Represents the simplest possible Group."""
+    """
+    Partial fragment from the 'groups' (root) node.
+    Represents the simplest possible Group.
+    """
 
     return {
         "id": "20EA715745389FCDED2C280A8ACB74A6",
@@ -108,6 +108,9 @@ def complex_group_dict():
                 "firstName": "Brendan",
                 "id": "6F63AF02CE05328153ABA477C76E6189",
                 "lastName": "Gleason",
+                "roles": [
+                    "29A7724B47ABEE7B3C9DC347E13A50B4",
+                ],
                 "subGroups": [
                     "BB6B3C3592C5FC71DBDD5258D45EF6D4",
                 ],
@@ -139,7 +142,13 @@ def test_from_dict_complex(complex_group_dict):
     ]
     assert sets_equal(public_attributes(my_sg), valid_attributes)
 
+    # Assertions for SpondGroup attributes
     assert my_sg.uid == "20EA715745389FCDED2C280A8ACB74A6"
     assert my_sg.name == "Group A"
+
+    # Assertions for SpondGroup.members -> SpondMember by attribute
     assert my_sg.members[0].uid == "6F63AF02CE05328153ABA477C76E6189"
+    assert my_sg.members[0].roles[0] == "29A7724B47ABEE7B3C9DC347E13A50B4"
+
+    # Assertions for SpondGroup.subgroups -> SpondSubgroup by attribute
     assert my_sg.subgroups[0].uid == "BB6B3C3592C5FC71DBDD5258D45EF6D4"
--- tests/utils.py
+++ tests/utils.py
@@ -9,14 +9,13 @@ from types import FunctionType
 
 def public_attributes(obj: object) -> list:
     members = getmembers(type(obj))
-    methods = {
-        name for name, value in members if isinstance(value, FunctionType)
-    }
+    methods = {name for name, value in members if isinstance(value, FunctionType)}
     return [
         name
         for name in dir(obj)
         if name[0] != "_" and name not in methods and hasattr(obj, name)
     ]
+
 
 def sets_equal(obj1: list, obj2: list) -> bool:
     return set(obj1) == set(obj2)
