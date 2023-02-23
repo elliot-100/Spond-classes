4460f8cc05c262f1a40d8ad415c9f4c850f282e0
Elliot <3186037+elliot-100@users.noreply.github.com>
2023-02-23 12:05:13 +0000

    Implement `SpondSubgroup.members` (#19)
    * add: implement `SpondSubgroup.members`
    
    * doc: README update

--- tests/test_spondgroup.py
+++ tests/test_spondgroup.py
@@ -153,7 +153,11 @@ def test_from_dict_complex(complex_group_dict):
 
     # Assertions for SpondGroup.subgroups -> SpondSubgroup by attribute
     assert my_sg.subgroups[0].uid == "BB6B3C3592C5FC71DBDD5258D45EF6D4"
+    # Test attributes not handled by simple SpondMember tests
+    assert my_sg.subgroups[0].members[0].uid == "6F63AF02CE05328153ABA477C76E6189"
 
     # Assertions by identity
     assert my_sg.members[0] in my_sg.members
     assert my_sg.members[0].subgroups[0] in my_sg.subgroups
+    assert my_sg.members[0] in my_sg.subgroups[0].members
+
