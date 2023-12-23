c42cd9f9891ade33451bcddff6da2d7c46f15183
Elliot <3186037+elliot-100@users.noreply.github.com>
2023-12-23 19:44:40 +0000

    Remove tests that didn't have value

--- tests/test_group.py
+++ tests/test_group.py
@@ -1,26 +1,15 @@
 """Tests for Group class."""
 
 from spond_classes import Group
-from tests.utils import public_attributes, sets_equal
 
 
 def test_from_dict(simple_group_data: dict) -> None:
     """Test that a Group is created from the simplest possible dict representation.
 
-    Verify that only expected attributes exist.
     Verify values of all attributes.
     """
-    valid_attributes = [
-        "uid",
-        "name",
-        "members",
-        "subgroups",
-        "roles",
-    ]
-    # act
     my_group = Group.core_from_dict(simple_group_data)
 
-    assert sets_equal(public_attributes(my_group), valid_attributes)
     assert my_group.uid == "20EA715745389FCDED2C280A8ACB74A6"
     assert my_group.name == "Group A"
     assert (
--- tests/test_member.py
+++ tests/test_member.py
@@ -3,31 +3,15 @@
 from datetime import datetime, timezone
 
 from spond_classes import Member
-from tests.utils import public_attributes, sets_equal
 
 
 def test_from_dict_simple(simple_member_data: dict) -> None:
     """Test that Member is created from dict.
 
-    Verify that only expected attributes exist.
     Verify values of all attributes.
     """
-    valid_attributes = [
-        "uid",
-        "created_time",
-        "email",
-        "first_name",
-        "last_name",
-        "full_name",
-        "phone_number",
-        "profile_uid",
-        "roles",
-        "subgroups",
-    ]
-    # act
     my_member = Member.from_dict(simple_member_data)
 
-    assert sets_equal(public_attributes(my_member), valid_attributes)
     assert my_member.uid == "6F63AF02CE05328153ABA477C76E6189"
     assert my_member.created_time == datetime(
         2022,
@@ -50,4 +34,5 @@ def test_from_dict_simple(simple_member_data: dict) -> None:
     assert (
         my_member.subgroups == []
     )  # Tested as part of complex Group, as it relies on full Group data.
+
     assert str(my_member) == "Member 'Brendan Gleason' (uid ends '...189')"
