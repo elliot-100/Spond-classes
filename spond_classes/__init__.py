3d817f3806912777e34eaa9875e8aebc9d8a2ba0
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-03-31 19:29:31 +0100

    Remove Pylint ignore

--- spond_classes/__init__.py
+++ spond_classes/__init__.py
@@ -1,6 +1,5 @@
 """Explicitly import all classes and functions into the package namespace."""
 
-# pylint: disable=useless-import-alias
 # `import x as x` pattern used here for explicit re-export for Mypy
 
 from .event import Event as Event
