f473fecb828190f7f59b330c45c4b373b8b69c45
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-07-09 18:47:50 +0100

    Mermaid ER diagram: current (key fields only)

--- spond_classes/__init__.py
+++ spond_classes/__init__.py
@@ -1,4 +1,56 @@
-"""Main module."""
+"""Main module.
+
+```mermaid
+
+erDiagram
+    GROUP {
+        str uid PK
+        inner_obj ROLE
+        inner_obj SUBGROUP
+        inner_obj MEMBER
+    }
+    MEMBER {
+        str uid PK
+        str subgroup_uid FK
+        str role_uid FK
+        obj profile "?"
+    }
+    ROLE {
+        str uid PK
+    }
+    SUBGROUP {
+        str uid PK
+    }
+    PROFILE {
+        str uid PK
+        obj member "?"
+    }
+    EVENT {
+        str uid PK
+        inner_obj responses
+    }
+    RESPONSES {
+        str accepted_uids FK "MEMBER"
+        str declined_uids FK "MEMBER"
+        str unanswered_uids FK "MEMBER"
+        str waiting_list_uids FK "MEMBER"
+        str unconfirmed_uids FK "MEMBER"
+    }
+
+    GROUP 1..0+ ROLE : contains
+    GROUP 1..0+ MEMBER : contains
+    GROUP 1..0+ SUBGROUP : contains
+
+    EVENT 1..1 RESPONSES : has
+
+    MEMBER 1..zero or one PROFILE : has
+    MEMBER 1..0+ ROLE : has
+    MEMBER 1..0+ SUBGROUP : has
+
+    RESPONSES 1..0+ MEMBER : contains
+```
+
+"""
 
 # Explicitly import all classes and functions into the package namespace.
 
