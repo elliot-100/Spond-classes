d2c144b2acb0f7b7611673a1ca1f12fdcd20fef0
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2025-03-05 18:17:01 +0000

    Role done

--- spond_classes/__init__.py
+++ spond_classes/__init__.py
@@ -20,19 +20,21 @@ These are the classes intended for direct user instantiation:
 # Explicitly import classes and functions into the package namespace to define the API.
 
 from .event import Event, EventType, Responses
-from .group import Group
-from .member import Member
-from .profile_ import Profile
+
+# from .group import Group
+# from .member import Member
+# from .profile_ import Profile
 from .role import Role
-from .subgroup import Subgroup
+
+# from .subgroup import Subgroup
 
 __all__ = [
     "Event",
     "EventType",
     "Responses",
-    "Group",
-    "Member",
-    "Profile",
+    # "Group",
+    # "Member",
+    # "Profile",
     "Role",
-    "Subgroup",
+    # "Subgroup",
 ]
--- spond_classes/role.py
+++ spond_classes/role.py
@@ -1,9 +1,16 @@
 """Module containing `Role` class."""
 
-from pydantic import BaseModel, Field
+from typing import Self
+
+from attrs import define, field
+from cattrs import Converter
+from cattrs.gen import make_dict_structure_fn
+
+from .types_ import DictFromJSON
 
 
-class Role(BaseModel):
+@define
+class Role:
     """Represents a role in the Spond system.
 
     A `Role` belongs to a `Group`.
@@ -11,10 +18,17 @@ class Role(BaseModel):
     Use `Group.members_by_role()` to get subordinate `Member`s.
     """
 
-    uid: str = Field(alias="id")
+    uid: str = field(alias="id")
     """`id` in Spond API; aliased as that's a Python built-in, and the Spond package
     uses `uid`."""
     name: str
+
+    @classmethod
+    def from_dict(cls, dict_: DictFromJSON) -> Self:
+        c = Converter()
+        hook = make_dict_structure_fn(cls, c, _cattrs_use_alias=True)
+        c.register_structure_hook(cls, hook)
+        return c.structure(dict_, cls)
 
     def __str__(self) -> str:
         """Return simple human-readable description."""
